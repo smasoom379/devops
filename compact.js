d697418e38befe2f66af668ad171f3180d92bc27
masoom <smasoom379@gmail.com>
2020-08-09 11:33:50 -0400

    Slave Branch changes

--- compact.js
+++ compact.js
@@ -15,5 +15,5 @@ alert( 'Hello, world! from Jerry changes' );
 alert( 'Hello, world! from Jerry changes' );
 alert( 'Hello, world! from Jerry changes' );
 alert( 'Hello, world! from Jerry changes' );
-Slave branch
-Slave branch
+Slave branch chnages 1
+Slave branch changes 2
